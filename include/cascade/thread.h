94cda490b18e0dd91b2ccf94b04437727d108898
Lee Cannon <leecannon@protonmail.com>
2026-06-07 00:06:43 +0100

    define `cascade_thread_handle`

--- include/cascade/thread.h
+++ include/cascade/thread.h
@@ -4,7 +4,14 @@
 #ifndef CASCADE_THREAD_H
 #define CASCADE_THREAD_H
 
+#include <stdint.h>
+
 #include <cascade/defines.h>
+
+typedef struct { uint64_t handle; } cascade_thread_handle;
+
+CASCADE_STATIC_ASSERT(sizeof(cascade_thread_handle) == sizeof(uint64_t), "thread handle size mismatch");
+CASCADE_STATIC_ASSERT(CASCADE_ALIGNOF(cascade_thread_handle) == CASCADE_ALIGNOF(uint64_t), "thread handle alignment mismatch");
 
 #ifdef __cplusplus
 extern "C" {
